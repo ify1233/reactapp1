c700a2222e74405c4703ad1511428db2d5149ea8
ify1233 <85139872+ify1233@users.noreply.github.com>
2021-10-06 05:49:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ console.log('ddd',tx)
       <br />
       <div>{check1}</div>
       </div>
-      <a className='btn btn-success' href="https://rinkeby.etherscan.io/address/"{check1} >CLICK TO VIEW CONTRACT ON ETHERSCAN</a>
+      <a className='btn btn-success' href={"https://rinkeby.etherscan.io/address/"$check1} >CLICK TO VIEW CONTRACT ON ETHERSCAN</a>
      
     </div>
   );
